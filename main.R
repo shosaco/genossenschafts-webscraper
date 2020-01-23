5f8fe2a15b8f2485d306b42cee2af9fbc3eb1083
Sandro Raabe <shosaco@users.noreply.github.com>
2020-01-23 09:19:54 +0100

    no input arguments

--- main.R
+++ main.R
@@ -1,24 +1,13 @@
-args = commandArgs(trailingOnly = TRUE)
-
-if (length(args) != 2) {
-  stop("Usage: Rscript main.R working-folder output-folder, where working-folder must contain script.R.")
-} else {
-  input = args[1]
-  if (!file.exists(input)) stop("Working folder ", input, " does not exist.")
-  
-  output = args[2]
-  if (!file.exists(output)) stop("Output folder ", output, " does not exist.")
+this_file = gsub("--file=", "", commandArgs()[grepl("--file", commandArgs())])
+if (length(this_file) == 0){
+  wd <- getwd()
+}else{
+  wd <- paste(head(strsplit(this_file, '[/|\\]')[[1]], -1), collapse = .Platform$file.sep)
 }
-
-out_file <- file(file.path(output, "GENOSSENSCHAFTEN_RESULT.txt"))
 
 # helper function because results come as UTF-8 but our strings are latin1
 tryCatch(
-  source(file.path(input, "script.r"), encoding = "UTF-8"),
-  error = function(e) writeLines(e$message, out_file),
-  finally = {
-    if (!is.null(results)) writeLines(results, out_file) else print("Nothing new.")
-    close(out_file)
-  }
+  source(file.path(wd, "script.r"), encoding = "UTF-8"),
+  error = function(e){ cat(toString(e)); Sys.sleep(600)}
 )
 
--- script.R
+++ script.R
@@ -1,5 +1,4 @@
 suppressPackageStartupMessages(library(rvest))
-results <- NULL  # toString(Sys.time())
 
 gatekeeper <- function(url){
   webpage <- tryCatch(read_html(url),
@@ -14,39 +13,39 @@ gatekeeper <- function(url){
 # BGS Fuhlsbüttel hat Angebote, falls der Text "keine ... verfügbar" nicht vorkommt
 l <- length(html_nodes(gatekeeper("https://www.bds-hamburg.de/unser-angebot/wohnungsangebote/"), ".ce0 p.gross"))
 if (l != 2){
-  results <- c(results, "BDS hat Angebote! Check https://www.bds-hamburg.de/unser-angebot/wohnungsangebote!")
+  browseURL("https://www.bds-hamburg.de/unser-angebot/wohnungsangebote")
 }
 
 # Kaifu hat Angebote, falls in .getRight h2 neue Überschriften, die wir nicht kennen, stehen
 offers <- unlist(as_list(html_nodes(gatekeeper("https://kaifu.de/index.php?id=14"), ".getRight .kaifu_elem .kaifu_content h2")))
-new <- offers[!grepl("12-108401-63|12-105204-95", offers)]
+new <- offers[!grepl("01-106003-66|12-108401-63|01-100801-62|12-105204-95|10-108301-24|01-100801-96", offers)]
 if(length(new) > 0){
-  results <- c(results, "Kaifu hat Angebote! Check https://kaifu.de/index.php?id=14")
+  browseURL("https://kaifu.de/index.php?id=14")
 }
 
 
 # Baugenossenschaft Fuhlsbüttel hat Angebote, falls der Link in result_list_... etwas anderes als das bekante Expose enthält
 res <- as_list(html_nodes(gatekeeper("https://portal.immobilienscout24.de/ergebnisliste/15339103"), ".result__list__element__infos--figcaption a"))
 links <- sapply(res, function(x) attr(x, "href"))
-if (!all(grepl("/expose/15339103/105323205/", links))){
-  results <- c(results, "Baugenossenschaft Fuhlsbüttel hat Angebote! Check https://portal.immobilienscout24.de/ergebnisliste/!")
+if (!all(grepl("/expose/15339103/107200431", links))){
+  browseURL("https://portal.immobilienscout24.de/ergebnisliste/15339103")
 }
 
 # Harabau hat Angebote, falls der Text "keine ... verfügbar" nicht vorkommt
 text <- html_nodes(gatekeeper("http://harabau.de/vermietung/wohnungen"), "#homepage-mietangebote-content #subtitle")  %>% html_text() %>% trimws()
 if(text != "Keine aktuellen Mietangebote verfügbar"){
-  results <- c(results, "Hamburg-Rahlstedter BGS hat Angebote! Check http://harabau.de/vermietung/wohnungen!")
+  browseURL("http://harabau.de/vermietung/wohnungen")
 }
 
 # Gartenstadt hat Angebote, falls der Text "keine Wohnungsangebote" nicht vorkommt
 text <- html_nodes(gatekeeper("https://www.gartenstadt-hamburg.de/angebote/"), "#main")  %>% html_text() %>% trimws()
 if(!grepl("Zur Zeit haben wir leider keine Wohnungsangebote", text)){
-  results <- c(results, "Gartenstadt Hamburg hat Angebote! Check https://www.gartenstadt-hamburg.de/angebote!")
+  browseURL("https://www.gartenstadt-hamburg.de/angebote")
 }
 
 # Farmsen hat Angebote, falls sie andere Angebote als das bekannte haben
 offers <- sapply(as_list(html_nodes(gatekeeper("https://www.mgf-farmsen.de/de/vermietungen"), ".immobilie .imm_top .imm_text a")), attr, "href")
-new <- offers[!grepl("obj=12", offers)]
+new <- offers[!grepl("[obj=8|obj=12]", offers)]
 if(length(new) > 0){
-  results <- c(results, "MGF Gartenstadt hat Angebote! Check https://www.mgf-farmsen.de/de/vermietungen!")
+  browseURL("https://www.mgf-farmsen.de/de/vermietungen")
 }
