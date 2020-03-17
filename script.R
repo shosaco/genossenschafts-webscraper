a6bb681f4bab607cd0786d7732bcfa058de3ed8b
Sandro Raabe <shosaco@users.noreply.github.com>
2020-03-17 14:31:05 +0100

    anpassungen

--- script.R
+++ script.R
@@ -4,21 +4,22 @@ gatekeeper <- function(url){
   webpage <- tryCatch(read_html(url),
                       error = function(e) cat(paste0("Fehler bei '", url, "': ", e)))
   if(is.null(webpage)){
-    stop(paste0("Fehler bei '", url, "'"))
+    stop("Fehler beim Aufrufen der Webseite", call. = F)
   }else{
     return(webpage)
   }
 }
 
-# BGS Fuhlsbüttel hat Angebote, falls der Text "keine ... verfügbar" nicht vorkommt
-l <- length(html_nodes(gatekeeper("https://www.bds-hamburg.de/unser-angebot/wohnungsangebote/"), ".ce0 p.gross"))
-if (l != 2){
+# BGS Fuhlsbüttel hat Angebote, falls es mehr als 2 die 2 bekannten Absätze gibt
+text_hp <- sapply(html_nodes(gatekeeper("https://www.bds-hamburg.de/unser-angebot/wohnungsangebote/"), ".ce0 p.gross"), html_text)
+
+if (sum(grepl("Sie sind auf der Suche|Aufgrund der vielen", text_hp)) > 2){
   browseURL("https://www.bds-hamburg.de/unser-angebot/wohnungsangebote")
 }
 
 # Kaifu hat Angebote, falls in .getRight h2 neue Überschriften, die wir nicht kennen, stehen
 offers <- unlist(as_list(html_nodes(gatekeeper("https://kaifu.de/index.php?id=14"), ".getRight .kaifu_elem .kaifu_content h2")))
-new <- offers[!grepl("01-106003-66|12-108401-63|01-100801-62|12-105204-95|10-108301-24|01-100801-96", offers)]
+new <- offers[!grepl("102401", offers) & !grepl("2 Zimmer", offers)]
 if(length(new) > 0){
   browseURL("https://kaifu.de/index.php?id=14")
 }
@@ -27,7 +28,8 @@ if(length(new) > 0){
 # Baugenossenschaft Fuhlsbüttel hat Angebote, falls der Link in result_list_... etwas anderes als das bekante Expose enthält
 res <- as_list(html_nodes(gatekeeper("https://portal.immobilienscout24.de/ergebnisliste/15339103"), ".result__list__element__infos--figcaption a"))
 links <- sapply(res, function(x) attr(x, "href"))
-if (!all(grepl("/expose/15339103/107200431", links))){
+new <- links[!grepl("116198180|", links)]
+if (length(new) > 0){
   browseURL("https://portal.immobilienscout24.de/ergebnisliste/15339103")
 }
 
