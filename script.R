3b0669fab86bc940c46bea219d4beaf302828882
Sandro Raabe <shosaco@users.noreply.github.com>
2020-08-24 15:31:07 +0200

    Hansa, VHW, BVE

--- script.R
+++ script.R
@@ -10,16 +10,17 @@ gatekeeper <- function(url){
   }
 }
 
-# BGS Fuhlsbüttel hat Angebote, falls es mehr als 2 die 2 bekannten Absätze gibt
-text_hp <- sapply(html_nodes(gatekeeper("https://www.bds-hamburg.de/unser-angebot/wohnungsangebote/"), ".ce0 p.gross"), html_text)
-
-if (sum(grepl("Sie sind auf der Suche|Aufgrund der vielen", text_hp)) > 2){
+# BDS hat Angebote, falls Immoliste mit neuen Angeboten befüllt ist
+offers <- html_nodes(gatekeeper("https://www.bds-hamburg.de/unser-angebot/wohnungsangebote/"), ".immobilielist .listitem a") %>% as_list %>% map_chr(attr, "href") %>% unique
+known <- "055.1.012"
+new <- offers[!grepl(paste(known, collapse = "|"), offers)]
+if (length(new) > 0){
   browseURL("https://www.bds-hamburg.de/unser-angebot/wohnungsangebote")
 }
 
 # Kaifu hat Angebote, falls in .getRight h2 neue Überschriften, die wir nicht kennen, stehen
 offers <- unlist(as_list(html_nodes(gatekeeper("https://kaifu.de/index.php?id=14"), ".getRight .kaifu_elem .kaifu_content h2")))
-new <- offers[!grepl("102401", offers) & !grepl("2 Zimmer", offers)]
+new <- offers[!grepl("106701|102501|105002|105201", offers) & !grepl("[1-3] Zimmer", offers)]
 if(length(new) > 0){
   browseURL("https://kaifu.de/index.php?id=14")
 }
@@ -28,7 +29,7 @@ if(length(new) > 0){
 # Baugenossenschaft Fuhlsbüttel hat Angebote, falls der Link in result_list_... etwas anderes als das bekante Expose enthält
 res <- as_list(html_nodes(gatekeeper("https://portal.immobilienscout24.de/ergebnisliste/15339103"), ".result__list__element__infos--figcaption a"))
 links <- sapply(res, function(x) attr(x, "href"))
-new <- links[!grepl("116198180|", links)]
+new <- links[!grepl("121803482", links)]
 if (length(new) > 0){
   browseURL("https://portal.immobilienscout24.de/ergebnisliste/15339103")
 }
@@ -40,8 +41,8 @@ if(text != "Keine aktuellen Mietangebote verfügbar"){
 }
 
 # Gartenstadt hat Angebote, falls der Text "keine Wohnungsangebote" nicht vorkommt
-text <- html_nodes(gatekeeper("https://www.gartenstadt-hamburg.de/angebote/"), "#main")  %>% html_text() %>% trimws()
-if(!grepl("Zur Zeit haben wir leider keine Wohnungsangebote", text)){
+text <- html_nodes(gatekeeper("https://www.gartenstadt-hamburg.de/angebote/"), "#main-content")  %>% html_text() %>% trimws()
+if(!grepl("keine Angebote zur Verfügung", text)){
   browseURL("https://www.gartenstadt-hamburg.de/angebote")
 }
 
@@ -50,4 +51,34 @@ offers <- sapply(as_list(html_nodes(gatekeeper("https://www.mgf-farmsen.de/de/ve
 new <- offers[!grepl("[obj=8|obj=12]", offers)]
 if(length(new) > 0){
   browseURL("https://www.mgf-farmsen.de/de/vermietungen")
+}
+
+# BVE
+library(purrr)
+offers <- as_list(html_nodes(gatekeeper("https://www.bve.de/wohnen-beim-bve/wohnungsbestand/wohnungsangebote/"), ".contentWrapper span")) %>% flatten
+if(length(offers) > 1 | offers[[1]] != "Zur Zeit sind leider keine Wohnungsangebote vorhanden."){
+  browseURL("https://www.bve.de/wohnen-beim-bve/wohnungsbestand/wohnungsangebote/")
+}
+
+# Hansa
+if(!grepl("Leider haben wir", html_nodes(gatekeeper("https://hansa-baugenossenschaft.de/wohnen/unsere-wohnungen"), "#c635") %>% html_text)){
+  browseURL("https://hansa-baugenossenschaft.de/wohnen/unsere-wohnungen")
+}
+
+# VHW
+offers <- html_nodes(gatekeeper("https://www.vhw-hamburg.de/wohnen/aktuelle-angebote.html"), ".searchResults--list--object--content h4 a") %>% as_list %>%  map_chr(attr, "href")
+known <- c("id-53201.4.1096", "id-53201.12.1050", "id-14203.2014.1083")
+new <- offers[!grepl(paste(known, collapse = "|"), offers)]
+if (length(new) > 0){
+  browseURL("https://www.vhw-hamburg.de/wohnen/aktuelle-angebote.html")
+}
+
+# WV1902
+if(!grepl("leider keine Wohnungen", html_nodes(gatekeeper("https://www.wv1902.de/Wohnungen"), "#Inhalt .text b") %>% html_text)){
+  browseURL("https://www.wv1902.de/Wohnungen")
+}
+
+# Süderelbe
+if(!grepl("keine freien Wohnungen", html_nodes(gatekeeper("https://www.baugen-suederelbe.de/wohnungangebote/"), ".page-content") %>% html_text())){
+  browseURL("https://www.baugen-suederelbe.de/wohnungangebote/")
 }
